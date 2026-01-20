f8cad4d7ac7783b9b994e49876c5c8c320fc90dd
StephenMuya <stephen2020mutheu@gmail.com>
2026-01-20 17:55:10 +0300

    Add RegimenCollection and animations for carousel functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import HeroSection from './Components/Hero';
 import MarqueeText from './Components/MarqueeText';
 import TrustBar from './Components/TrustBar';
 import FeaturedCollections from './Components/FeaturedCollections';
+import RegimenCollection from './Components/RegimenCollection';
+import TheJournal from './Components/TheJournal';
+import Newsletter from './Components/Newsletter';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
         <MarqueeText />
         <TrustBar />
         <FeaturedCollections />
+        <RegimenCollection />
+        <TheJournal />
+        <Newsletter />
       </main>
+      <Footer />
     </>
   );
 }
